ddfda8ef4e815a88b84eacc521dc6b452578dcf6
Lee Yanyu Elena <2006.leeyanyu@gmail.com>
2025-11-18 20:59:14 +0800

    popup window

---
+++ colourIT.py
@@ -0,0 +1,29 @@
+import pygame
+
+WIDTH, HEIGHT = 900, 600
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Colour IT!")
+
+WHITE = (255, 255, 255)
+
+FPS = 15
+
+def draw_window():
+    WINDOW.fill(WHITE)
+    pygame.display.update()
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run: 
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw_window()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
