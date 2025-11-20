a154c6d879ec8d18749aae8d27d7e02e5ccf84a0
Lydia <creeperace123@gmail.com>
2025-11-20 22:56:02 +0800

    Player placeholder added with simple horizontal movement and falling physics.

--- colourIT.py
+++ colourIT.py
@@ -10,12 +10,63 @@ WHITE = (255, 255, 255)
 
 FPS = 15
 
+PLAYER_VEL = 5
 
-def draw_window():
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+    GRAVITY = 1
+#PLAYER SPRITTE
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+#MOVEMENT FUNC
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.move(self.x_vel, self.y_vel)
+
+        self.fall_count += 1
+        
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
+
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+
+    player.x_vel = 0
+    if keys[pygame.K_a]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_d]:
+        player.move_right(PLAYER_VEL)
+
+def draw_window(player):
     WINDOW.fill(WHITE)
+    player.draw(WINDOW)
     pygame.display.update()
 
 def main():
+    player = Player(100, 100, 50, 50)
+    
     clock = pygame.time.Clock()
     run = True
     while run: 
@@ -24,17 +75,21 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window()
+#Input/Movement
+        handle_move(player)
+#Updates player position
+        player.loop(FPS)
+#Drawing Everything
+        draw_window(player)
 
     pygame.quit()
 
 #javia 
-def update_world():
+#def update_world():
 
 
 #lydia 
-def update_mobs():
-
+#def update_mobs():
 
 if __name__ == "__main__":
     main()
